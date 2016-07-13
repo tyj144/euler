3d563ff8311b2a6086745b6efae66cbf6666430b
Tyler Jiang <tylerjiang@Tylers-MacBook-Pro.local>
2016-07-13 17:48:53 -0400

    Cleaned up code

--- 001_multiples_3_5.py
+++ 001_multiples_3_5.py
@@ -1,6 +1,9 @@
-sum = 0
-for num in range(1000):
-	if (num % 3 == 0 or num % 5 == 0):
-		sum += num
+print(sum(num for num in range(1000) if (num % 3 == 0 or num % 5 == 0)))
 
-print(sum)
+# Old Solution
+# sum = 0
+# for num in range(1000):
+# 	if (num % 3 == 0 or num % 5 == 0):
+# 		sum += num
+# 
+# print(sum)
