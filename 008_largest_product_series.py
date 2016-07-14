4366060370d5141947a406ccaf79afbb02ef9b59
Tyler Jiang <tylerjiang@Tylers-MacBook-Pro.local>
2016-07-14 14:19:09 -0400

    Solved eighth problem

---
+++ 008_largest_product_series.py
@@ -0,0 +1,15 @@
+large_num = open('./large_num.txt', 'r').read()
+largest_product = 0
+
+for start in range(len(large_num)):
+	product = 1
+	if start + 13 <= len(large_num) - 1:
+		number_snippet = large_num[start:start+13]
+	else: break
+	
+	for digit in number_snippet:
+		product *= int(digit)
+	if product > largest_product:
+		largest_product = product
+		
+print(largest_product)
